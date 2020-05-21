f6b3b398e33cc41b3a261126826f005ec08dc9be
Zucce05 <Zucce05@hotmail.com>
2020-05-21 14:24:39 -0600

    Add project files.

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selection Sort");
+        }
+    }
+}
